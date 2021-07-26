2e5f33cefdb6cf725acacc9e5fb51391a59f3291
Agri Edd. Oematan <dnr739528@gmail.com>
2021-07-26 21:43:27 +0800

    done sarana seeder

--- database/factories/SaranaFactory.php
+++ database/factories/SaranaFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Sarana::class, function (Faker $faker) {
     return [
-        //
+        'nama'      => $faker->colorName(),
+        'jumlah'    => $faker->randomNumber(5),
+        'kondisi'   => $faker->randomElement(['baik', 'rusak ringan', 'rusak sedang', 'rusak berat']),
     ];
 });
--- database/seeds/SaranaSeeder.php
+++ database/seeds/SaranaSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Prasarana;
+use App\Sarana;
 use Illuminate\Database\Seeder;
 
 class SaranaSeeder extends Seeder
@@ -9,8 +11,11 @@ class SaranaSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run(){
+        Prasarana::get()->each(function($prasarana){
+            factory(Sarana::class, rand(1, 4))->create([
+                'id_prasarana'  => $prasarana->id_prasarana
+            ]);
+        });
     }
 }

5ae303b2d85b60a340d2a773d40feda64594d716
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-13 19:05:20 +0800

    copy guru page to pimpinan page

---
+++ resources/js/pages/pimpinan/PimpinanBase.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-view/>
+        <guru-tambah/>
+        <guru-ubah/>
+        <guru-hapus/>
+    </div>
+</template>
+<script>
+import GuruHapus from './PimpinanHapus.vue'
+import GuruTambah from './PimpinanTambah.vue'
+import GuruUbah from './PimpinanUbah.vue'
+export default {
+    components: { 
+        GuruTambah, 
+        GuruUbah, 
+        GuruHapus 
+    },
+}
+</script>
---
+++ resources/js/pages/pimpinan/PimpinanUbah.vue
@@ -0,0 +1,111 @@
+<template>
+    <div>
+        <v-dialog v-model="dialog" max-width="600" content-class="shadow-sm" overlay-opacity=".25" eager scrollable persistent>
+            <v-form @submit.prevent="submit" :disabled="loading">
+                <v-card>
+                    <v-toolbar flat>
+                        <v-subheader>
+                            Form Ubah Data Guru > {{ item.nama }}
+                        </v-subheader>
+                        <v-spacer/>
+                        <v-avatar color="grey lighten-3">
+                            <v-icon>mdi-school</v-icon>
+                        </v-avatar>
+                    </v-toolbar>
+                    <v-divider/>
+                    <v-card-text v-if="dialog && exists">
+                        <form-tambah-guru v-model="item" :errors="errors"/>
+                    </v-card-text>
+                    <v-card-text v-else-if="dialog && loading">
+                        <form-tambah-guru-placeholder/>
+                    </v-card-text>
+                    <v-divider/>
+                    <v-card-actions>
+                        <v-btn text @click="dialog = false" :loading="loading">
+                            Batal
+                        </v-btn>
+                        <v-spacer/>
+                        <v-btn text color="primary" type="submit" :loading="loading">
+                            Simpan
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-form>
+        </v-dialog>
+    </div>
+</template>
+<script>
+import { mapActions, mapMutations, mapState } from 'vuex'
+import FormTambahGuru from './form/FormTambahPimpinan.vue'
+import FormTambahGuruPlaceholder from './form/FormTambahGuruPlaceholder.vue'
+export default {
+    components: { FormTambahGuru, FormTambahGuruPlaceholder },
+    data(){
+        return {
+            loading: false,
+            errors: {},
+            item: {},
+            ori: {},
+            exists: false,
+        }
+    },
+    computed: {
+        ...mapState({
+            value_dialog: state => state.guru.modal.ubah,
+            id: state => state.guru.selected.id
+        }),
+        dialog: {
+            get(){ return this.value_dialog },
+            set(value){ this.setDialog({value}) }
+        }
+    },
+    methods: {
+        ...mapActions({
+            setDialog: 'guru/setModalUbah',
+            updateGuru: 'guru/update',
+            findGuru: 'guru/show',
+            updateSession: 'guru/updateSession',
+            notif: 'notifikasi/show'
+        }),
+        async submit(e){
+            let data = new FormData(e.target)
+            this.loading = true
+            let res = await this.updateGuru({ data, id: this.id }).catch(e => {
+                console.log("updateGuru@GuruUbah.vue", e)
+                e.response.status == 422 && this.setErrorForm(e)
+                this.notif({
+                    message: e.message
+                })
+            })
+            this.loading = false
+            if(res){
+                this.updateSession()
+                this.dialog = false
+            }
+        },
+        async loadItem(){
+            this.exists = false
+            this.loading = true
+            let res = await this.findGuru({ id: this.id }).catch(e => {
+                console.log("loadItem@GuruUbah.vue", e);
+                this.notif({
+                    message: e.message
+                })
+            })
+            this.loading = false
+            if(res){
+                this.exists = true
+                for(let key in res.data.data){
+                    this.$set(this.item, key, res.data.data[key])
+                    this.$set(this.ori, key, res.data.data[key])
+                }
+            }
+        }
+    },
+    watch: {
+        value_dialog(val){
+            val && this.$nextTick(e => this.loadItem())
+        }
+    }
+}
+</script>

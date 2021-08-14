44492db31865ea4bdbff5ffef03613fdf7ddbccd
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-14 08:31:26 +0800

    copy guru page to berita page

---
+++ resources/js/pages/berita/GuruBase.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-view/>
+        <guru-tambah/>
+        <guru-ubah/>
+        <guru-hapus/>
+    </div>
+</template>
+<script>
+import GuruHapus from './BeritaHapus.vue'
+import GuruTambah from './BeritaTambah.vue'
+import GuruUbah from './BeritaUbah.vue'
+export default {
+    components: { 
+        GuruTambah, 
+        GuruUbah, 
+        GuruHapus 
+    },
+}
+</script>

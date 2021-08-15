ea214af796b792b5c0834349662b2e399cc30300
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-15 21:49:32 +0800

    copy guru to kegiatan

---
+++ resources/js/pages/kegiatan/KegiatanBase.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-view/>
+        <guru-tambah/>
+        <guru-ubah/>
+        <guru-hapus/>
+    </div>
+</template>
+<script>
+import GuruHapus from './KegiatanHapus.vue'
+import GuruTambah from './KegiatanTambah.vue'
+import GuruUbah from './KegiatanUbah.vue'
+export default {
+    components: { 
+        GuruTambah, 
+        GuruUbah, 
+        GuruHapus 
+    },
+}
+</script>

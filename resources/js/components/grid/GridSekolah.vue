59614755eae47ac573ea1480e6fbd49b2aaded77
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-17 16:01:19 +0800

    dashboard

---
+++ resources/js/components/grid/GridSekolah.vue
@@ -0,0 +1,12 @@
+<template>
+	<v-card flat color="grey lighten-5" rounded="xl">
+		<v-card-text>
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, itaque tempora. Minima eum expedita, hic voluptas totam reiciendis, quidem ex adipisci cum possimus incidunt ratione ad tenetur voluptatem quis dolor.
+		</v-card-text>
+	</v-card>
+</template>
+<script>
+export default {
+	
+}
+</script>
--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -5,17 +5,15 @@
         </v-app-bar>
         <v-main>
             <div class="d-grid-dashboard pa-3">
-                <v-card flat color="grey lighten-5" rounded="xl" v-for="i in 10" :key="i">
-                    <v-card-text>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, itaque tempora. Minima eum expedita, hic voluptas totam reiciendis, quidem ex adipisci cum possimus incidunt ratione ad tenetur voluptatem quis dolor.
-                    </v-card-text>
-                </v-card>
+                <grid-sekolah/>
             </div>
         </v-main>
     </div>
 </template>
 <script>
+import GridSekolah from '../components/grid/GridSekolah.vue'
 export default {
+  components: { GridSekolah },
     
 }
 </script>

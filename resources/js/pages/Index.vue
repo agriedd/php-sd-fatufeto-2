a98b7a71f7e72be6c0c9d23b8c574e14e5c338b1
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-17 15:29:05 +0800

    added migration

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -4,7 +4,13 @@
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </v-app-bar>
         <v-main>
-            
+            <div class="d-grid-dashboard pa-3">
+                <v-card flat color="grey lighten-5" rounded="xl" v-for="i in 10" :key="i">
+                    <v-card-text>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, itaque tempora. Minima eum expedita, hic voluptas totam reiciendis, quidem ex adipisci cum possimus incidunt ratione ad tenetur voluptatem quis dolor.
+                    </v-card-text>
+                </v-card>
+            </div>
         </v-main>
     </div>
 </template>

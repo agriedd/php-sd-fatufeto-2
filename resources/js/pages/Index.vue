76277b99e38b50545ac4e6ccb9298f67f26586c4
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-18 19:18:02 +0800

    absolute app bar index admin

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <v-app-bar title="Panel Admin" app flat floating>
+        <v-app-bar title="Panel Admin" app flat floating absolute>
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
         </v-app-bar>
         <v-main>

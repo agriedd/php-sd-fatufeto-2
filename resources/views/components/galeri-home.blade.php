aa80d76656c77fe40dbc1447bfe5662b90ceeeb1
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-20 11:13:56 +0800

    update galeri public scss

--- resources/views/components/galeri-home.blade.php
+++ resources/views/components/galeri-home.blade.php
@@ -1,15 +1,19 @@
-<div class="mb-3 py-3" id="galeri">
-    <h4>
+<div class="mb-3 py-5 text-center" id="galeri">
+    <h3 class="mb-0">
         Galeri
-    </h4>
+    </h3>
+    <small class="text-muted">
+        Galeri Kegiatan {{ env('APP_NAME') }}
+    </small>
+    <div class="mx-auto my-2" style="border-bottom: 3px solid var(--bs-teal); max-width: 2rem"></div>
 </div>
 <div class="mb-3">
     @php
-        $list = ['large', null, 'wide', null, 'large'];
+        $list = ['large', null, null, 'wide', null, null, 'large'];
     @endphp
     <div class="galery-home">
         @foreach ($list_galeri as $index => $galeri)
-            <div class="{{ isset($list[$index]) && $list[$index] != null ? $list[$index] : null }}" style="background-image: url('{{ $galeri->foto->url }}');"></div>
+            <a href="#" class="{{ isset($list[$index]) && $list[$index] != null ? $list[$index] : null }}" style="background-image: url('{{ $galeri->foto->url }}');"></a>
         @endforeach
     </div>
 </div>

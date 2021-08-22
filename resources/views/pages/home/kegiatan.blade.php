45f8bf7b3aad7767c4fb948ab1513b99c64e047d
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-22 20:31:24 +0800

    improvement kegiatan

--- resources/views/pages/home/kegiatan.blade.php
+++ resources/views/pages/home/kegiatan.blade.php
@@ -26,7 +26,7 @@
             </div>
             <div class="col-lg-3">
                 <div class="sticky-top px-3 pb-3" style="z-index: 0; top: 80px">
-                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.05); border-radius: .5rem">
+                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.025); border-radius: 1rem">
                         <div class="pe-4 d-flex flex-column justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-flag-fill" viewBox="0 0 16 16">
                                 <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
@@ -41,7 +41,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.05); border-radius: .5rem">
+                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.025); border-radius: 1rem">
                         <div class="pe-4 d-flex flex-column justify-content-center position-relative">
                             <div class="text-light">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-calendar-event-fill" viewBox="0 0 16 16">
@@ -68,7 +68,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.05); border-radius: .5rem">
+                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.025); border-radius: 1rem">
                         <div class="pe-4 d-flex flex-column justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
@@ -83,7 +83,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.05); border-radius: .5rem">
+                    <div class="d-flex p-3 mb-2" style="background: rgba(0,0,0,.025); border-radius: 1rem">
                         <div class="pe-4 d-flex flex-column justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
                                 <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>

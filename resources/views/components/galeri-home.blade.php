89ce1040d40812027d457da216edf8318bf13fbd
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-22 14:15:01 +0800

    fixed conflig galeri-home

--- resources/views/components/galeri-home.blade.php
+++ resources/views/components/galeri-home.blade.php
@@ -12,17 +12,8 @@
         $list = ['large', null, null, 'wide', null, null, 'large'];
     @endphp
     <div class="galery-home">
-<<<<<<< HEAD
         @foreach ($list_galeri as $index => $galeri)
             <a href="#" class="{{ isset($list[$index]) && $list[$index] != null ? $list[$index] : null }}" style="background-image: url('{{ $galeri->foto->url }}');"></a>
         @endforeach
-=======
-        <div class="large" style="background-image: url('{{ asset('img/index.jpg') }}');"></div>
-        <div style="background-image: url('{{ asset('img/dua.jpg') }}');"></div>
-        <div style="background-image: url('{{ asset('img/tiga.jpg') }}');"></div>
-        <div class="wide" style="background-image: url('{{ asset('img/empat.jpg') }}');"></div>
-        <div style="background-image: url('{{ asset('img/ryan-ancill-aJYO8JmVodY-unsplash.jpg') }}');"></div>
-        <div class="large" style="background-image: url('{{ asset('img/samuel-regan-asante-4UWfXZpQM4k-unsplash.jpg') }}');"></div>
->>>>>>> 7c4e6f6 (ketik pesan)
     </div>
 </div>

4935e442be4835d4d1560f0e56172908dac68d78
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-22 20:38:24 +0800

    improvement kelas

--- resources/views/components/list-kelas.blade.php
+++ resources/views/components/list-kelas.blade.php
@@ -1,13 +1,13 @@
 <div>
-    <div class="grid-kelas">
+    <div class="grid-kelas py-4">
         @forelse ($list_kelas as $item)
             <a href="{{ route('home.siswa', [
                 'kelas' => urlencode($item->nama),
                 'id_kelas' => $item->id_kelas
             ]) }}" class="text-decoration-none">
-                <div class="card td-none text-dark h-100" style="border-radius: 1rem">
-                    <div class="card-body">
-                        <h5 class="text-uppercase">
+                <div class="card td-none text-dark h-100 border-0" style="border-radius: 2rem; background-color: rgba(0, 0, 0, .025)">
+                    <div class="card-body p-4">
+                        <h5 class="text-uppercase2">
                             {{ $item->nama }}
                         </h5>
                         <div>

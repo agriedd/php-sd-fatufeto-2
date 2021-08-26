2e322f9bc1617c6b2bad75c107f500af8e72ba95
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-26 10:34:08 +0800

    fixed api guru

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,22 @@ Route::prefix('v1')->group(function(){
         Route::post('authorize', 'api\v1\UserController@authorization');
     });
 
+    
+    Route::group(['prefix' => '/guru', 'middleware' => ['auth:api_guru']], function(){
+        Route::group(['prefix' => 'user'], function(){
+            /**
+             * authentikasi
+             * 
+             */
+            Route::post('authorize', 'api\v1\UserController@authorization');
+        });
+        Route::resource('jadwal', 'api\v1\JadwalController');
+        Route::resource('kelas', 'api\v1\KelasController');
+        Route::resource('siswa', 'api\v1\SiswaController');
+        Route::resource('kegiatan', 'api\v1\KegiatanController');
+        Route::resource('guru', 'api\v1\GuruController');
+    });
+
     Route::resource('sekolah', 'api\v1\SekolahController');
     Route::put('guru/reset/{guru}', 'api\v1\GuruController@reset');
     Route::resource('guru', 'api\v1\GuruController');
@@ -42,19 +58,5 @@ Route::prefix('v1')->group(function(){
     Route::resource('kategori', 'api\v1\KategoriController');
     Route::resource('kegiatan.gambar', 'api\v1\GambarKegiatanController');
 
-    Route::middleware('auth:api_guru')->group(['prefix' => '/guru'], function(){
-        Route::group(['prefix' => 'user'], function(){
-            /**
-             * authentikasi
-             * 
-             */
-            Route::post('authorize', 'api\v1\UserController@authorization');
-            Route::resource('jadwal', 'api\v1\JadwalController');
-            Route::resource('kelas', 'api\v1\KelasController');
-            Route::resource('siswa', 'api\v1\SiswaController');
-            Route::resource('kegiatan', 'api\v1\KegiatanController');
-        });
-        Route::resource('guru', 'api\v1\GuruController');
-    });
 
 });

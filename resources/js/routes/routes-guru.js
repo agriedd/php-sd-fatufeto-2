c4cd7d99c315e4f9a0bd2bc8e9548bca8f310b9d
Agri Edd. Oematan <dnr739528@gmail.com>
2021-08-31 17:49:24 +0800

    guru siswa

--- resources/js/routes/routes-guru.js
+++ resources/js/routes/routes-guru.js
@@ -1,9 +1,10 @@
 import Index from '../pages/dashboard/guru/Index.vue'
 import Kelas from '../pages/dashboard/guru/tab/GuruKelasTab.vue'
+import Siswa from '../pages/dashboard/guru/tab/GuruSiswaTab.vue'
 
 export default [
     { path: '/u/guru', component: Index, children: [
-        { path: 'siswa', component: Kelas, name: 'siswa' },
+        { path: 'siswa', component: Siswa, name: 'siswa' },
         { path: '', component: Kelas, name: 'dashboard' }
     ] },
 ]

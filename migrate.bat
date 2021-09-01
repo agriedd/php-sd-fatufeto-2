6032e9a87cc05d688ec7a9b805fdadff9811154e
Agri Edd. Oematan <dnr739528@gmail.com>
2021-09-01 17:18:58 +0800

    migrate wali kelas only

--- migrate.bat
+++ migrate.bat
@@ -1,4 +1,4 @@
 start php artisan migrate
-start php artisan db:seed --class=GuruSeeder
-start php artisan db:seed --class=KelasSiswaSeeder
+@REM start php artisan db:seed --class=GuruSeeder
+@REM start php artisan db:seed --class=KelasSiswaSeeder
 start php artisan db:seed --class=WalikelasSeeder

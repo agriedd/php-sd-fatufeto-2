204842a353f05cbc11c6bf59c1133b29d83e9220
Agri Edd. Oematan <dnr739528@gmail.com>
2021-10-22 06:45:14 +0800

    update migration file

--- migrate.bat
+++ migrate.bat
@@ -1,4 +1,4 @@
 start php artisan migrate
 @REM start php artisan db:seed --class=GuruSeeder
 @REM start php artisan db:seed --class=KelasSiswaSeeder
-start php artisan db:seed --class=WalikelasSeeder
+@REM start php artisan db:seed --class=WalikelasSeeder

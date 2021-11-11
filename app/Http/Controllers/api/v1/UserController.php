74848c105cf286e171ee17283beb2c88676441a5
agriedd <dnr739528@gmail.com>
2021-11-11 15:36:37 -0800

    okd

--- app/Http/Controllers/api/v1/UserController.php
+++ app/Http/Controllers/api/v1/UserController.php
@@ -13,10 +13,10 @@ class UserController extends Controller
     public function authorization(Request $request){
         if(Auth::guard('api')->check())
             return new AdminCollection(Auth::guard('api')->user());
-        if(Auth::guard('api_guru')->check())
-            return new GuruCollection(Auth::guard('api_guru')->user());
         if(Auth::guard('api_pimpinan')->check())
             return new GuruCollection(Auth::guard('api_pimpinan')->user());
+        if(Auth::guard('api_guru')->check())
+            return new GuruCollection(Auth::guard('api_guru')->user());
         return response([
             "data"      => null,
             "message"   => "Masalah autentikasi"
